c5dd1aeaef9c9ba3a29c4994337928a2be5479d3
tamim <tamimcmrd@gmail.com>
2022-09-13 19:30:28 +0600

    done

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,14 @@
-import {getApp, getApps, initializeApp} from 'firebase/app'
-import {getFirestore} from 'firebase/firestore'
-import {getStorage} from 'firebase/storage'
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: "AIzaSyBKrAtujm8_SECqj48NymNifRNA6y_HoqA",
-  authDomain: "ecommerce-first-prac.firebaseapp.com",
-  databaseURL: "https://ecommerce-first-prac-default-rtdb.firebaseio.com",
-  projectId: "ecommerce-first-prac",
-  storageBucket: "ecommerce-first-prac.appspot.com",
-  messagingSenderId: "876923548883",
-  appId: "1:876923548883:web:b34ed4ad1a387e8f865026"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESEEGINGSENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = getApps.length> 0 ? getApp(): initializeApp (firebaseConfig);
@@ -16,4 +16,4 @@ const app = getApps.length> 0 ? getApp(): initializeApp (firebaseConfig);
 const firestore = getFirestore(app)
 const storage = getStorage(app);
 
-export {app, firestore, storage}
+export { app, firestore, storage };
